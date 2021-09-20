d6e2bd2bfee13db69a6d857714ca0614d8f0557c
maheshboin <87657449+maheshboin@users.noreply.github.com>
2021-09-20 16:59:48 +0530

    firstcommit

---
+++ WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Value A"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Value B"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+        </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add Them" />
+            <asp:Label ID="Label3" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Subtract Them" />
+            <asp:Label ID="Label4" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Multiply Them" />
+            <asp:Label ID="Label5" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Divide Them" />
+            <asp:Label ID="Label6" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" style="height: 29px" Text="Complete All Calculations" />
+        </p>
+        <p>
+            <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" style="margin-bottom: 0px" Text="Clear All Values" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            decimal decValueA = 0m;
+            decimal decValueB = 0m;
+            decimal decAdd = 0m;
+            decValueA = Convert.ToDecimal(TextBox1.Text);
+            decValueB = Convert.ToDecimal(TextBox2.Text);
+            decAdd = decValueA + decValueB;
+            Label3.Text = Convert.ToString(decAdd);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            decimal decValueA = 0m;
+            decimal decValueB = 0m;
+            decimal decSub = 0m;
+            decValueA = Convert.ToDecimal(TextBox1.Text);
+            decValueB = Convert.ToDecimal(TextBox2.Text);
+            decSub = decValueA - decValueB;
+            Label4.Text = Convert.ToString(decSub);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            decimal decValueA = 0m;
+            decimal decValueB = 0m;
+            decimal decMul = 0m;
+            decValueA = Convert.ToDecimal(TextBox1.Text);
+            decValueB = Convert.ToDecimal(TextBox2.Text);
+            decMul = decValueA * decValueB;
+            Label5.Text = Convert.ToString(decMul);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal decValueA = 0m;
+                decimal decValueB = 0m;
+                decimal decDiv = 0m;
+                decValueA = Convert.ToDecimal(TextBox1.Text);
+                decValueB = Convert.ToDecimal(TextBox2.Text);
+                decDiv = decValueA / decValueB;
+                Label6.Text = Convert.ToString(decDiv);
+            }
+            catch
+            {
+                Label6.Text = "CANT DIVIDE BY ZERO";
+            }
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            decimal decValueA = 0m;
+            decimal decValueB = 0m;
+            decimal decC = 0m;
+            decValueA = Convert.ToDecimal(TextBox1.Text);
+            decValueB = Convert.ToDecimal(TextBox2.Text);
+            decC = decValueA + decValueB;
+            Label3.Text = Convert.ToString(decC);
+            decC = decValueA -decValueB;
+            Label4.Text = Convert.ToString(decC);
+            decC = decValueA * decValueB;
+            Label5.Text = Convert.ToString(decC);
+            decC = decValueA / decValueB;
+            Label6.Text = Convert.ToString(decC);
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            Label3.Text = "";
+            Label4.Text = "";
+            Label5.Text = "";
+            Label6.Text = "";
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+
+        }
+    }
+}
