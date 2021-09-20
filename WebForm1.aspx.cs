322c89563861ce20d2890026f79dfb28c1a2bfe1
maheshboin <87657449+maheshboin@users.noreply.github.com>
2021-09-20 17:05:45 +0530

    c3

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -10,7 +10,7 @@ namespace WebApplication4
     public partial class WebForm1 : System.Web.UI.Page
     {
 
-        protected void Page_Load(object sender, EventArgs e
+        protected void Page_Load(object sender, EventArgs 
         {
 
         }
